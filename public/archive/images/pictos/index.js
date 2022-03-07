ecc2cd8977da36670db87f880fa71f14e46bd4db
m4rrc0 <marcoet@gmail.com>
2022-03-07 16:09:40 +0100

    WIP: add archived static pages

---
+++ public/archive/images/pictos/index.js
@@ -0,0 +1,19 @@
+import animator from './animator'
+import drinks from './drinks'
+import food from './food'
+import handicapped from './handicapped'
+import snacks from './snacks'
+import vegan from './vegan'
+import vegetarian from './vegetarian'
+import children from './children'
+
+export default {
+  animator,
+  drinks,
+  food,
+  handicapped,
+  snacks,
+  vegan,
+  vegetarian,
+  children,
+}

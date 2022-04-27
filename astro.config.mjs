f10b0922965ce2300b043c9c4b6e394193e6f7e4
m4rrc0 <marcoet@gmail.com>
2022-04-27 17:58:56 +0200

    update astro@beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+// https://astro.build/config
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // @type-check enabled!
@@ -7,24 +9,17 @@ import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
 
-// REMARK: was necessary as Astro wasn't handling private env vars properly
-// import dotenv from 'dotenv'
-// import fs from 'fs/promises'
-// const env = dotenv.parse(await fs.readFile('.env'))
-
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  // Comment out "renderers: []" to enable Astro's default component support.
-  // renderers: [],
+export default defineConfig({
+  // your configuration options here...
+  site: 'https://www.wanna-play.be/',
+  // trailingSlash: 'always', // 'always' is the default
+  integrations: [
+    sitemap({
+      filter: (page) => page !== 'https://www.wanna-play.be/styleguide',
+    }),
+  ],
   vite: {
-    // REMARK: was necessary as Astro wasn't handling private env vars properly
-    // expose private env var on the server only
-    // as seen on https://github.com/withastro/astro/issues/1765
-    // define: {
-    //   'process.env.DIRECTUS_URL': `"${env.DIRECTUS_URL}"`,
-    //   'process.env.DIRECTUS_EMAIL': `"${env.DIRECTUS_EMAIL}"`,
-    //   'process.env.DIRECTUS_PW': `"${env.DIRECTUS_PW}"`,
-    // },
     // NOTE: necessary for astro-icon apparently (https://github.com/natemoo-re/astro-icon)
     ssr: {
       external: ['svgo'],

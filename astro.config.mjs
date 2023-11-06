4f11143a4833e092bacad56149c733a0a2d77c44
Cyril <cyril.lefebvre@soprasteria.com>
2023-11-06 22:24:50 +0100

    FEAT: small cleaning

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,7 @@ import { defineConfig } from "astro/config"
 import svelte from "@astrojs/svelte"
 import sitemap from "@astrojs/sitemap"
 import robotsTxt from "astro-robots-txt"
-// import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import critters from "astro-critters"
-// import { astroImageTools } from "astro-imagetools"
-// import compress from "astro-compress"
 
 const PAGE_ADMIN = process.env.PAGE_ADMIN || null
 
@@ -40,51 +37,6 @@ export default defineConfig({
       logger: 1, // default is 2
       fonts: true,
       exclude: [(file) => file.startsWith("./dist/fr/e")],
-    }),
-    // image({
-    //   serviceEntryPoint: "@astrojs/image/sharp",
-      // supported levels: 'debug' | 'info' | 'warn' | 'error' | 'silent' / default: 'info'
-      // logLevel: "debug",
-      // may be useful if your hosting provider allows caching between CI builds
-    //   cacheDir: "./_cache/images",
-    // }),
-    // astroImageTools,
-    // compress({
-    //   css: false,
-
-    //   // html: {
-    //   //   // minify: true,
-    //   //   collapseInlineTagWhitespace: false,
-    //   //   // collapseWhitespace: false,
-    //   //   decodeEntities: false,
-    //   //   removeComments: true,
-    //   // },
-    //   js: false,
-    //   img: false,
-    //   svg: false,
-    // }),
-    // minifyHtml(),
-  ],
-  // vite: {
-  //   // NOTE: necessary for astro-icon apparently (https://github.com/natemoo-re/astro-icon)
-  //   ssr: {
-  //     external: ["svgo"],
-  //   },
-  //   // plugins: [viteCommonjs()],
-  // },
+    })
+  ]
 })
-
-// ?? Should I integrate postCSS config here? postCSS + Svelte?
-//
-// import presetEnv from 'postcss-preset-env'
-// import cssNano from 'cssnano'
-// const postCssConfig = { plugins: [presetEnv, cssNano] }
-// export default defineConfig({
-//   integrations: [svelte()],
-//   postcss: postCssConfig,
-//   vite: {
-//     css: {
-//       postcss: postCssConfig,
-//     },
-//   }
-// }

788fbc4fa72c985443b0422ac105a271b0a3b396
jenny <fernandezgarcia.jenny@gmail.com>
2024-08-14 12:50:38 +0200

    test non concluant notion API

---
+++ src/services/notion.js
@@ -0,0 +1,19 @@
+const NOTION_URL = import.meta.env.NOTION_URL || process.env.NOTION_URL;
+const NOTION_API = import.meta.env.NOTION_SECRET || process.env.NOTION_SECRET;
+// The things I need for my request
+const axios = require('axios');
+const { Client } = require("@notionhq/client");
+const notion = new Client({auth:NOTION_API});
+
+async function loadNotionPage() {
+    await axios.get(NOTION_URL)
+    .then((page) =>{
+        // console.log(page);//for debug
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+    
+}
+
+loadNotionPage()

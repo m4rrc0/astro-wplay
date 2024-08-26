8cc1e10b8905b4fa0946e1f6e84f88cf26f4e6cf
m4rrc0 <marcoet@gmail.com>
2024-08-26 15:55:07 +0200

    fix(global): fix missing first char in markdown descriptions. Parse with marked directly. Remove astro-remote.

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,12 @@
-export { default as SEO } from './SEO.astro'
+export { default as SEO } from "./SEO.astro"
 // export { default as Blocks } from './Blocks.svelte'
-export { default as Blocks } from './Blocks.astro'
-export { default as Block } from './Block.astro'
-export { default as ListOrNot } from './ListOrNot.astro'
-export { default as Md } from './Md.astro'
-export { default as IO } from './IO.astro'
-export { default as ImgDirectus } from './ImgDirectus.astro'
-export { default as Img } from './Img.astro'
+export { default as Blocks } from "./Blocks.astro"
+export { default as Block } from "./Block.astro"
+export { default as ListOrNot } from "./ListOrNot.astro"
+export { default as Markdown } from "./Markdown.astro"
+export { default as Md } from "./Md.astro"
+export { default as IO } from "./IO.astro"
+export { default as ImgDirectus } from "./ImgDirectus.astro"
+export { default as Img } from "./Img.astro"
 // export { default as Mailto } from './Mailto.astro'
-export { default as MtObf } from './MtObf.astro'
+export { default as MtObf } from "./MtObf.astro"

2179668011ad2ecf683a36322770a5d76b0a90ed
m4rrc0 <github-h9s2@m4rr.co>
2026-03-10 09:33:09 +0100

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,15 @@ import robotsTxt from 'astro-robots-txt'
 import critters from 'astro-critters'
 import icon from 'astro-icon'
 import { PAGE_ADMIN } from './src/utils/env'
-import Biome from '@playform/format'
+// import Biome from '@playform/format'
 
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
 
+const isProd = process.env.NODE_ENV === 'production'
+console.log('NODE_ENV: ', process.env.NODE_ENV)
 // @ts-check
 export default defineConfig({
   site: 'https://www.wanna-play.be/',
@@ -25,7 +27,7 @@ export default defineConfig({
     domains: ['cms.wanna-play.be'],
   },
   integrations: [
-    Biome(),
+    // Biome(),
     svelte(),
     sitemap({
       filter: page =>
@@ -42,11 +44,16 @@ export default defineConfig({
         },
       ],
     }),
-    (await import('astro-critters')).default({
-      logger: 1, // default is 2
-      fonts: true,
-      exclude: [file => file.startsWith('./dist/fr/e')],
+    (await import('@playform/inline')).default({
+      Logger: isProd ? 0 : 2, // default is 2
+      Fonts: true, // Not sure this is still working or needed...
+      Exclude: [file => /dist\/fr\/e/.test(file)],
     }),
+    // (await import('astro-critters')).default({
+    //   logger: 1, // default is 2
+    //   fonts: true,
+    //   Exclude: [file => /dist\/fr\/e/.test(file)],
+    // }),
     // critters({
     //   logger: 1, // default is 2
     //   fonts: true,
